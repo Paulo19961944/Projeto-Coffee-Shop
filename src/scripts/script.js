c9b539cb15690e814c3ff1881385058a9dc3b244
Paulo19961944 <paulo24041996@gmail.com>
2024-04-21 15:45:52 -0300

    Projeto Cafeteria - V1.0.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,20 @@
-const bodyElement = window.document.body
-abrirMenuMobile.addEventListener ('click', abrirMenu)
-fecharMenuMobile.addEventListener('click', fecharMenu)
+const bodyElement = window.document.body // Captura o Body
+abrirMenuMobile.addEventListener ('click', abrirMenu)  // Adiciona evento ao Botão Abrir Mobile
+fecharMenuMobile.addEventListener('click', fecharMenu) // Adiciona evento ao Botão Fechar Mobile
 
+// Função para abrir o menu
 function abrirMenu(){
-    bodyElement.style.overflow = 'hidden'
-    menu.style.display = 'flex'
-    menu.style.flexDirection = 'column'
-    menu.style.alignItems = 'center'
-    menu.style.justifyContent = 'center'
-    menu.style.gap = '48px'
-    abrirMenuMobile.style.display = 'none'
-    fecharMenuMobile.style.display = 'flex'
+    bodyElement.style.overflow = 'hidden' // Trava o Body
+    menu.style.display = 'flex' // Flexbox
+    menu.style.flexDirection = 'column' // Alinha em Colunas
+    menu.style.alignItems = 'center' // Alinha ao Centro no Eixo Perpendicular
+    menu.style.justifyContent = 'center' // Alinha ao Centro no Eixo Principal
+    menu.style.gap = '48px' // Espaçamento de 48px
+    abrirMenuMobile.style.display = 'none' // Esconde o Botão Abrir Mobile
+    fecharMenuMobile.style.display = 'flex' // Aparece o Botão Fechar Mobile
 }
 
+// Função para fechar o menu
 function fecharMenu(){
-    window.location.reload()
+    window.location.reload() // Recarrega a Página
 }
